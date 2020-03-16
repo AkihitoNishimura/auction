04a94156839b3807e2cf0ca4aa04313736b43feb
AkihitoNishimura <plumizreop.3@gmail.com>
2020-03-17 08:40:55 +0900

    add バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 50}
+  validates :description, presence: true, length: {maximum: 2000}
+  validates :price, presence: true, numericality: {only_integer: true,greater_than_or_equal_to:1}
+  validates :seller, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :seller, presence: true, format: {width: VALID_EMAIL_REGEX} 
+  VALID_IMAGE_REGEX=/\.(jpg|jpeg|png|gif|bmp)\z/i
+  validates :seller, presence: true, format: {width: VALID_IMAGE_REGEX}
 end
