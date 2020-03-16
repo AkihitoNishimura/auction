ab986f7b74ac1057613aead96318f16b191d0484
AkihitoNishimura <plumizreop.3@gmail.com>
2020-03-17 07:43:34 +0900

    add destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,12 @@ class ItemsController < ApplicationController
     redirect_to @item
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
   #private以下はこのクラス外での編集不可になる(privateメソッド)
   private
 
